8ba513cf0977dea9959379ca6e78b54eaa99d5e4
kiwi <kiwi.swhite.coder@gmail.com>
2012-04-23 15:27:02 +0800

    delete directory first if clone to existing directory

--- src/repository-clone-init.rb
+++ src/repository-clone-init.rb
@@ -1,5 +1,6 @@
-require 'colorize'
 require File.dirname(__FILE__) + '/repository-clone'
+require 'rubygems'
+require 'colorize'
 
 repositories = File.open(File.dirname(__FILE__) + "/../data/java_repository.data").readlines
 
@@ -12,9 +13,15 @@ seed_range = 3
 seed_from.upto(seed_from+seed_range) {
 	|index|
 
+	#if certain directory has already exist, remove the directory first
+	dir_exist = `ls #{repositories[index].chomp} 2> /dev/null`
+	if (dir_exist != "")
+		'rm -rf #{repositories[index].chomp}'
+	end
+
 	STDOUT.write "clone #{repositories[index]}".yellow
 	repos_clone.clone(repositories[index].chomp)
-	
+
 	seed = File.open(File.dirname(__FILE__) + "/../data/seed.tmp", 'w')
 	seed.puts "#{index+1}"
 	seed.close
