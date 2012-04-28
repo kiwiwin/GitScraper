8b98cc73f5ddf90274f57c45a355acd26532b136
kiwi <kiwi.swhite.coder@gmail.com>
2012-04-28 10:49:46 +0800

    remove commit analysis

--- test/user-scraper-tests.rb
+++ test/user-scraper-tests.rb
@@ -17,7 +17,6 @@ describe UserScraper do
 	let(:dummy_page_index) {1}
 
 	context "blank user page" do
-
 		before (:all) do
 			@blank_page_scraper = create_scraper_of_stub_url("blank")
 		end
@@ -37,7 +36,6 @@ describe UserScraper do
 	end
 
 	context "half repositories page" do
-
 		before (:all) do
 			@half_page_scraper = create_scraper_of_stub_url("half")
 			@half_user_list = load_user_list("half")
@@ -54,11 +52,9 @@ describe UserScraper do
 		it "all users equal to half_user_list" do
 			@half_page_scraper.all_elements.should == @half_user_list
 		end
-
 	end
 
 	context "full users page" do
-
 		before (:all) do
 			@full_page_scraper = create_scraper_of_stub_url("full")
 			@full_user_list = load_user_list("full")
@@ -73,10 +69,9 @@ describe UserScraper do
 		end
 
 		it "all users equal to full_repository_list" do
+# too large for test
 #			@full_page_scraper.all_elements.should == @full_repository_list * 2
 		end
-
 	end
-
 end
 
