2bd9169eced1bd545ceb752b21ecdf06383b8133
kiwi <kiwi.swhite.coder@gmail.com>
2012-04-28 11:18:17 +0800

    merge stub-search-url into scraper-tests-helper

--- test/scraper-tests-helper.rb
+++ test/scraper-tests-helper.rb
@@ -1,11 +1,13 @@
 require File.dirname(__FILE__) + "/../src/user-scraper"
 require File.dirname(__FILE__) + "/../src/repository-scraper"
-require File.dirname(__FILE__) + "/stub-search-url"
+require File.dirname(__FILE__) + "/../src/search-url"
 
 class ScraperTestsHelper
+
 	def self.create_scraper_of_stub_url(class_name, fixture_type)
 		fixture_name = "#{fixture_type}_#{class_name.split("Scraper")[0].downcase}_page.html"
-		stub_url = StubSearchURL.new("#{File.dirname(__FILE__)}/fixture/#{fixture_name}")		
+		stub_url = StubSearchURL.new("#{File.dirname(__FILE__)}/fixture/#{fixture_name}")
+	
 		scraper = eval(class_name).new(stub_url) 
 	end
 
@@ -15,5 +17,20 @@ class ScraperTestsHelper
  			|line|
 			line.chomp
 		}
+	end
+
+end
+
+class StubSearchURL < SearchURL
+	def initialize(filename)
+		@filename = filename
+	end
+
+	def search_url
+		@filename
+	end
+
+	def single_page_url(page_index)
+		@filename
 	end
 end
