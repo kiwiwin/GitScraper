4b4dad959d39470aa7e7d74b008fdef6618b283c
kiwi <kiwi.swhite.coder@gmail.com>
2012-10-31 13:54:02 +0800

    add ghost method in SearchUrl

--- spec/search_url_spec.rb
+++ spec/search_url_spec.rb
@@ -3,11 +3,11 @@ require_relative "spec_helper"
 describe SearchURL do
 	
 	it "verify search_users_url whose location is china" do
-			SearchURL.new.search_url(:Users, :location => "china").should == "https://github.com/search?&q=location%3Achina&type=Users"
+			SearchURL.new.search_users_url(:location => "china").should == "https://github.com/search?&q=location%3Achina&type=Users"
 	end
 
 	it "verify search_repositories_url whose language is csharp and size is 10" do
-			SearchURL.new.search_url(:Repositories, :language => "Csharp", :size => 10).should == "https://github.com/search?&q=language%3ACsharp+size%3A10&type=Repositories"
+			SearchURL.new.search_repositories_url(:language => "Csharp", :size => 10).should == "https://github.com/search?&q=language%3ACsharp+size%3A10&type=Repositories"
 	end
 
 end
--- src/search_url.rb
+++ src/search_url.rb
@@ -11,4 +11,9 @@ class SearchURL
 	def criteria(param)
 			param.to_a.inject([]) { |res, pair| res << pair.join("%3A") }.join('+')
 	end
+
+	def method_missing(method_id, *params)
+		return search_url($1.capitalize, params[0]) if method_id.to_s =~ /^search_(.*)_url$/
+		super
+	end
 end
